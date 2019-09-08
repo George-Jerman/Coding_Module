3c68d167cad7c05a3ab7b0f265c1533f6abae3df
George-Jerman <georgejerman@gprotonmail.com>
2019-09-08 12:30:48 +0100

    Added addition functionality

--- test/test.c
+++ test/test.c
@@ -4,6 +4,15 @@
 int main()
 {
 	printf("Hello World\n");
+    
+    float x;
+    float y;
+    float z;
+    printf("What would you like to add together? \n");
+    scanf("%f", &y);
+    scanf("%f", &z);
+    x = y + z;
+    printf("The answer is %f \n" , x);
 	return 0;
 }
 
