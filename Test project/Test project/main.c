c9d3045240c3d234f062e7a640cc1a35d8826e5c
George-Jerman <georgejerman@gprotonmail.com>
2019-09-09 16:28:20 +0100

    Changed basic adder into a unit converter. Still requires error handling etc

--- Test project/Test project/main.c
+++ Test project/Test project/main.c
@@ -1,17 +1,84 @@
 #include <stdio.h>
 #include <math.h>
 
+/* Program to do simple unit conversions from a list given */
+
+//function declarations
+float Temp_fc(void);
+float Temp_cf(void);
+float Mass_pk(void);
+float Mass_kp(void);
+
+
 int main()
 {
-    printf("Hello World\n");
-    
-    float x;
-    float y;
-    float z;
-    printf("What would you like to add together? \n");
-    scanf("%f", &y);
-    scanf("%f", &z);
-    x = y + z;
-    printf("The answer is %f \n" , x);
+    int input_choice;
+    printf("What would you like to convert? \n");
+    printf("1. Farenheit to Celsius \n 2. Celsius to Farenheit \n 3. Pounds to Kilograms \n 4. Kilograms to Pounds \n ");
+    scanf("%d", &input_choice );
+    switch (input_choice) {
+        case 1:
+            Temp_fc();
+            break;
+        case 2:
+            Temp_cf();
+            break;
+        case 3:
+            Mass_pk();
+            break;
+        case 4:
+            Mass_kp();
+            break;
+        default:
+            return 1;
+            break;
+    }
+}
+//farenheit to celsius
+float Temp_fc()
+{
+    float tbc;
+    float converted_val;
+    printf("Please input the value you wish to convert \n");
+    scanf("%f", &tbc);
+    converted_val = ((tbc - 32) * 5) /9;
+    printf("Your value is %f \n", converted_val);
     return 0;
+    
+}
+//celsius to farenheit
+float Temp_cf()
+{
+    float tbc;
+    float converted_val;
+    printf("Please input the value you wish to convert \n");
+    scanf("%f", &tbc);
+    converted_val = ((tbc * 9) /5) + 32;
+    printf("Your value is %f \n", converted_val);
+    return 0;
+    
+}
+//pounds to kilos
+float Mass_pk()
+{
+    float tbc;
+    float converted_val;
+    printf("Please input the value you wish to convert \n");
+    scanf("%f", &tbc);
+    converted_val = tbc / 2.2;
+    printf("Your value is %f \n", converted_val);
+    return 0;
+    
+}
+//kilos to pounds
+float Mass_kp()
+{
+    float tbc;
+    float converted_val;
+    printf("Please input the value you wish to convert \n");
+    scanf("%f", &tbc);
+    converted_val = tbc * 2.2;
+    printf("Your value is %f \n", converted_val);
+    return 0;
+    
 }
