91e98afcbed225cced82f4bb5603652ec1dea384
George-Jerman <georgejerman@gprotonmail.com>
2019-10-17 09:52:26 +0100

    Added Frobenius norm capability

--- Assignment_1/Assignment_1/main.c
+++ Assignment_1/Assignment_1/main.c
@@ -11,9 +11,9 @@
 #include <getopt.h>
 #include <math.h>
 #include <string.h>
+#include <math.h>
 
 #define MAX_LINE_LENGTH 10000
-#define MAX_MATRICES 3
 
 
 /* Constants for signalling errors: */
@@ -35,7 +35,7 @@ double ** CreateArray1(int rows, int columns);
 double** CreateArray2(int rows2, int columns2);
 double** Populate_mat1(double** mat1_placeholder, FILE *matrix_1,int rows,int columns);
 double** Populate_mat2(double** mat2_placeholder, FILE *matrix_2,int rows2,int columns2);
-static Error frobenius_norm(void);
+static Error frobenius_norm(double ** mat1, int rows, int columns);
 static Error transpose(void);
 static Error matrix_product(void);
 static Error determinant(void);
@@ -49,7 +49,7 @@ int main(int argc, char ** argv)
     char * input_file_name_1 = NULL;
     char * input_file_name_2 = NULL;
     
-    int rows, columns, rows2, columns2;
+    int rows = 0, columns = 0, rows2 = 0, columns2= 0;
     FILE *matrix_1;
     FILE *matrix_2;
     
@@ -69,7 +69,7 @@ int main(int argc, char ** argv)
     /* getopt_long needs somewhere to store its option index. */
     int option_index = 0;
         
-    int c = getopt_long( argc, argv, ":z:x:f:t:m:d:a:i:", long_options, &option_index );
+    int c = getopt_long( argc, argv, ":z:x:ftm:d:a:i:", long_options, &option_index );
     /* End of options is signalled with '-1' */
     while (c != -1) {
         switch (c) {
@@ -83,6 +83,7 @@ int main(int argc, char ** argv)
                 double **mat1_placeholder =CreateArray1(rows, columns);
                 printf("Matrix1\n");
                 double **mat1 =Populate_mat1(mat1_placeholder, matrix_1, rows, columns);
+                free(mat1_placeholder);
                 break;
             case 'x': //when file2 is specified creates an array from the data in input file specified
                 input_file_name_2 = optarg;
@@ -94,9 +95,14 @@ int main(int argc, char ** argv)
                 double **mat2_placeholder =CreateArray2(rows2, columns2);
                 printf("Matrix2\n");
                 double **mat2 =Populate_mat2(mat2_placeholder, matrix_2, rows2, columns2);
+                free(mat2_placeholder);
                 break;
             case 'f':
-                ret_val = frobenius_norm();
+                if (rows ==0 || columns ==0) {
+                    printf("You need to specify the input file before attempting to pick an operation to do on it.\n");
+                    return BAD_FORMAT;
+                }
+                return frobenius_norm(mat1, rows, columns);
                 break;
             case 't':
                 ret_val = transpose();
@@ -124,7 +130,7 @@ int main(int argc, char ** argv)
                 fprintf(stderr, "Warning: option '-%c' is invalid: ignored\n", optopt);
                 break;
         }
-        c = getopt_long( argc, argv, ":z:x:f:t:m:d:a:i:", long_options, &option_index );
+        c = getopt_long( argc, argv, ":z:x:ft:m:d:a:i:", long_options, &option_index );
     }
     return NO_ERROR;
 }
@@ -238,11 +244,11 @@ double** Populate_mat1(double** mat1_placeholder, FILE *matrix_1,int rows,int co
     }
     for (i=0; i<rows; i++) {
         for (j=0; j<columns; j++) {
-            f = fscanf(matrix_1, "%lf", &mat1_placeholder[i][j]);
+            f = fscanf(matrix_1, "%lg", &mat1_placeholder[i][j]);
             if (f != 1) {
                 exit(BAD_FORMAT);
             }
-            printf("%lf\t", mat1_placeholder[i][j]);
+            printf("%.13lg\t", mat1_placeholder[i][j]);
         }
         printf("\n");
     }
@@ -276,11 +282,11 @@ double** Populate_mat2(double** mat2_placeholder, FILE *matrix_2,int rows2,int c
     }
     for (i=0; i<rows2; i++) {
         for (j=0; j<columns2; j++) {
-            f = fscanf(matrix_2, "%lf", &mat2_placeholder[i][j]);
+            f = fscanf(matrix_2, "%lg", &mat2_placeholder[i][j]);
             if (f != 1) {
                 exit(BAD_FORMAT);
             }
-            printf("%lf\t", mat2_placeholder[i][j]);
+            printf("%.13g\t", mat2_placeholder[i][j]);
         }
         printf("\n");
     }
@@ -288,8 +294,18 @@ double** Populate_mat2(double** mat2_placeholder, FILE *matrix_2,int rows2,int c
     return mat2_placeholder;
 }
 
-static Error frobenius_norm(void)
+static Error frobenius_norm(double ** mat1, int rows, int columns)
 {
+    int i,j;
+    double element=0,sum=0;
+    for (i=0; i<rows; i++) {
+        for (j=0; j<columns; j++) {
+            element = pow(mat1[i][j], 2);
+            sum += element;
+        }
+    }
+    sum = sqrt(sum);
+    printf("The frobenius norm of the matrix is: %.13lg\n", sum);
     return NO_ERROR;
 }
 
